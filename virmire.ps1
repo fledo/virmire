318150f8f596f7271e96b3620b0cc3f204807f0a
fledo <fred.uggla@gmail.com>
2014-05-27 11:23:31 +0200

    Initial commit

---
+++ virmire.ps1
@@ -0,0 +1,338 @@
+﻿$FolderIcon = @"
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+	public class IconExtractor
+	{
+
+	 public static Icon Extract(string file, int number, bool largeIcon)
+	 {
+	  IntPtr large;
+	  IntPtr small;
+	  ExtractIconEx(file, number, out large, out small, 1);
+	  try
+	  {
+	   return Icon.FromHandle(largeIcon ? large : small);
+	  }
+	  catch
+	  {
+	   return null;
+	  }
+
+	 }
+	 [DllImport("Shell32.dll", EntryPoint = "ExtractIconExW", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+	 private static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
+
+	}
+}
+"@
+
+Function Update-ButtonContent {
+    param (
+        $Button
+    )
+    if ($Button.Status -eq $True) {
+        if (Test-Path -Path $Button.target -PathType Leaf) {
+            $Button.Image = [System.Drawing.Icon]::ExtractAssociatedIcon($Button.Target)
+        } else {
+            $Button.Image = [System.IconExtractor]::Extract("shell32.dll", 4, $true)
+        }
+        $Button.Text = "$($Button.name)`n`n`n`n`n`n$([io.path]::GetFileNameWithoutExtension($Button.Target))"
+    }
+}
+
+Function Choose-File {
+    param (
+        $Button
+    )
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    if ($OpenFileDialog.ShowDialog() -eq "OK") {
+        $Button.Target = $OpenFileDialog.FileName
+        $Button.Status = $True
+        Update-ButtonContent -Button $Button
+    } else {
+        $Button.Target = "null"
+        $Button.Status = $False
+        $Button.Image = $null
+    }
+} 
+
+Function Choose-Folder {
+    param (
+        $Button
+    )
+    $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    if ($OpenFolderDialog.ShowDialog() -eq "OK") {
+        $Button.Target = $OpenFolderDialog.SelectedPath
+        $Button.Status = $True
+        Update-ButtonContent -Button $Button
+    } else {
+        $Button.Target = "null"
+        $Button.Status = $False
+        $Button.Image = $null
+    }
+}
+
+Function Save-Data {
+    $Output = @()
+    Foreach ($object in $Buttons) {
+        $Output += New-Object -TypeName PSObject -Property @{Key=$object.Key; Target=$object.Target; Status = $object.Status}
+    }
+    $Output | Export-Csv $DataFile -Force
+    Load-Listener
+}
+
+Function Show-GUI {
+
+    # Form Base 
+    $Form = New-Object system.Windows.Forms.Form
+    $Form.FormBorderStyle = 'None'
+    $Form.Width = 1110
+    $Form.Height = 380
+    $Form.StartPosition = "CenterScreen"
+    $Form.BackColor = [System.Drawing.Color]::DarkGray
+
+    $Font = New-Object System.Drawing.Font("Arial",18)
+
+    # Label
+    $title = New-Object System.Windows.Forms.Label
+    $title.Text = "Choose a hotkey for CTRL + ALT"
+    $title.Width = 450
+    $title.Height = 50
+    $title.Font = $Font
+    $title.TextAlign = [System.Drawing.ContentAlignment]::MiddleCenter
+    $Form.Controls.Add($title)
+
+    # Keyboard buttons
+    $left = 10
+    $top = 50
+    foreach ($object in $Data) {
+        if ($object.Key -eq "A") {
+        $left = 30
+        $top  += 110
+    } elseif ($object.Key -eq "Z") {
+        $left = 70
+        $top  += 110
+    }
+        $button = New-Object System.Windows.Forms.Button
+        $button | Add-Member -NotePropertyName Target -NotePropertyValue $object.Target
+        $button | Add-Member -NotePropertyName Status -NotePropertyValue $object.Status
+        $button | Add-Member -NotePropertyName Key -NotePropertyValue $object.Key
+        $button.name = $object.Key
+        $button.add_MouseDown({
+            if ($this.Status -eq $False) {
+                if ($_.button -eq "Left") {
+                    Choose-File -Button $this
+                } elseif ($_.button -eq "Right") {
+                    Choose-Folder -Button $this
+                }
+            } else {
+                $this.Target = "null"
+                $this.Status = $False
+                $this.Image = $null
+                $this.Text = $this.name
+            }
+        })
+        $button.Text = $object.Key
+        $button.Width = 100
+        $button.Height = 100
+        $button.Top = $top
+        $button.Left = $left
+        $button.Padding = 1
+        $button.BackColor = [System.Drawing.Color]::LightGray
+        $button.TextAlign = [System.Drawing.ContentAlignment]::TopLeft
+        $button.ImageAlign = [System.Drawing.ContentAlignment]::MiddleLeft
+        Update-ButtonContent -Button $button
+        $Form.Controls.Add($button)
+        $global:Buttons += $button
+        $left += 110
+    }
+    
+    $help = New-Object System.Windows.Forms.Button
+    $help.BackColor = [System.Drawing.Color]::LightGray
+    #$help.FlatStyle = [System.Windows.Forms.FlatStyle]::Popup
+    $help.Text = "Help"
+    $help.Width = 100
+    $help.Height = 25
+    $help.Top = 10
+    $help.Left = 450
+    $help.Add_Click({
+        $popup = new-object -comobject wscript.shell
+        $popup.popup("hej")
+    })
+    $Form.Controls.Add($help)
+    
+    $remove = New-Object System.Windows.Forms.Button
+    $remove.BackColor = [System.Drawing.Color]::LightGray
+    $remove.Text = "Reset keys"
+    $remove.Width = 100
+    $remove.Height = 25
+    $remove.Top = 10
+    $remove.Left = 560
+    $remove.Add_Click({
+        $YesNo = new-object -comobject wscript.shell
+        $intAnswer = $YesNo.popup("Do you want to remove all configured hotkeys?", 0, "Remove hotkeys", 4)
+        If ($intAnswer -eq 6) {
+          Set-Defaults
+          $Form.Close()
+          $YesNo.popup("Configured keys removed. This program will now close.")
+        }
+    })
+    $Form.Controls.Add($remove)
+    
+    $start = New-Object System.Windows.Forms.Button
+    $start.BackColor = [System.Drawing.Color]::LightGray
+    $start.ForeColor = [System.Drawing.Color]::DarkGreen
+    $start.Text = "Start Listener"
+    $start.Width = 100
+    $start.Height = 25
+    $start.Top = 10
+    $start.Left = 670
+    $start.Add_Click({
+        Load-Listener
+    })
+    $Form.Controls.Add($start)
+    
+    $stop = New-Object System.Windows.Forms.Button
+    $stop.BackColor = [System.Drawing.Color]::LightGray
+    $stop.ForeColor = [System.Drawing.Color]::DarkRed
+    $stop.Text = "Stop Listener"
+    $stop.Width = 100
+    $stop.Height = 25
+    $stop.Top = 10
+    $stop.Left = 780
+    $stop.Add_Click({
+        Kill-Listener
+    })
+    $Form.Controls.Add($stop)
+    
+    $save = New-Object System.Windows.Forms.Button
+    $save.BackColor = [System.Drawing.Color]::LightGray
+    $save.Text = "Save"
+    $save.Width = 100
+    $save.Height = 25
+    $save.Top = 10
+    $save.Left = 890
+    $save.Add_Click({
+        Save-Data
+        [System.Windows.Forms.MessageBox]::Show("Data saved to $DataFile")
+    })
+    $Form.Controls.Add($save)
+    
+    $exit = New-Object System.Windows.Forms.Button
+    $exit.BackColor = [System.Drawing.Color]::LightGray
+    $exit.Text = "Exit"
+    $exit.Width = 100
+    $exit.Height = 25
+    $exit.Top = 10
+    $exit.Left = 1000
+    $exit.Add_Click({
+        $Form.Close()
+    })
+    $Form.Controls.Add($exit)
+    
+    $Form.ShowDialog() > $null
+}
+
+Function Kill-Listener {
+    sleep -Milliseconds 1000
+    $id = Get-Content $PidFile -ErrorAction SilentlyContinue
+    if ($id) {
+        $Process = Get-Process -id $id -ErrorAction SilentlyContinue
+        if (($Process.name -eq "powershell") -and ($id -eq $Process.Id)) {
+            Stop-Process -id $id
+        }
+    }
+}
+
+function Set-Defaults {
+    Kill-Listener
+    Remove-Item $Appdata -Force -Recurse -ErrorAction SilentlyContinue
+    New-Item $Appdata -type directory -Force > $null
+    $Data = @()
+    $Keys = ("Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M")
+    foreach ($key in $Keys) {
+        $Data += New-Object -TypeName PSObject -Property @{Key=$key; Target="null"; Status = $False}
+    }
+    $Data | Export-Csv $DataFile -Force
+}
+
+<#
+    .SYNOPSIS
+    Tries to load a module.
+
+    .DESCRIPTION
+    Loads the module from the parameter or displays an
+    error message with a link where it can be downloaded.
+
+    .PARAMETER Module
+    Specifies the module to be loaded.
+
+    .PARAMETER Web
+    Specifies where the module can be downloaded.
+   
+    .PARAMETER Name
+    Specifies the name of the running script which requires the module
+
+    .EXAMPLE
+    Load-Module -Module ShowUI -Web showui.codeplex.com -Name $MyInvocation.MyCommand.Name
+#>
+function Load-Module {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Module,
+        [Parameter(Mandatory=$true)]
+        [string]$Web,
+        [string]$Name = "This script"
+    )
+    if (-not (Get-Module $Module))
+    {
+        try {
+            Import-Module $Module -Force -ErrorAction Stop
+        } catch {
+            write-host "`nError: $_" -ForegroundColor Red
+            write-host "$Name requires '$Module' to function properly. Please download it from " -NoNewLine -ForegroundColor Red
+            write-host "$Web`n" -BackgroundColor Blue
+            Exit
+        }
+    }
+}
+
+# (re)load the listener file/process
+function Load-Listener {
+    # Make sure we have the required module and that the previous Listener is dead
+    Load-Module -Module pseventingplus -Web pseventing.codeplex.com/releases/view/66587 -Name "The background Listener process"   
+    Kill-Listener
+    $Data = Import-Csv $DataFile
+    "import-module pseventingplus" | Out-File -FilePath $ListenerFile -Force
+    Foreach ($object in $Data) {
+        if ($object.Status -eq $True){
+            Add-Content -Value "register-hotkeyevent 'ctrl+alt+$($object.Key)' -action  { start '$($object.Target)' } -global" -Path $ListenerFile
+        }
+    }
+    Add-Content -Value 'write-host "Keys registered. Do not close this window.`nPID: $pid. Saved to $env:APPDATA\Keys\pid.txt"' -Path $ListenerFile
+    Add-Content -Value '$pid | Out-File -FilePath "$env:APPDATA\Keys\pid.txt" -Force' -Path $ListenerFile
+    
+    # Start the Listener. Keep alive and hidden.
+    start-process powershell.exe -WindowStyle Hidden -argument '-NoExit -nologo -noprofile -executionpolicy bypass -command . $env:APPDATA\Keys\listener.ps1' 
+}
+
+$global:Buttons = @()
+$Appdata = "$env:APPDATA\Keys"
+$DataFile = "$Appdata\data.csv"
+$PidFile = "$Appdata\pid.txt"
+$ListenerFile = "$Appdata\listener.ps1"
+if (-not (Test-Path $Appdata)) {
+    Set-Defaults
+}
+$Data = Import-Csv $DataFile
+
+Add-Type -TypeDefinition $FolderIcon -ReferencedAssemblies System.Drawing
+Add-Type -AssemblyName System.Windows.Forms
+
+Show-GUI
